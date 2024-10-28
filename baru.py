1b7e874b423e7afac16677fdb37d1fdb92bc529c
rootmika <69230864+rootmika@users.noreply.github.com>
2024-10-28 08:50:57 +0700

    headless

--- baru.py
+++ baru.py
@@ -2,21 +2,24 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Set up the WebDriver (Chrome in this example)
-driver_path = r"C:\Users\lenovo\Downloads\ISC\briforce\vote\chromedriver.exe"
-driver = webdriver.Chrome(service=Service(driver_path))
+driver_path = r"C:\Users\UTSUKIRA\Downloads\impstr\chromedriver.exe"
+chrm_option = Options()
+chrm_option.add_argument('--headless=new')
+driver = webdriver.Chrome(service=Service(driver_path),options = chrm_option)
 # driver = webdriver.Chrome()  # Make sure you have the ChromeDriver in your PATH or specify its path
 # driver.maximize_window()
 
 try:
     # Navigate to the target URL
-    print(0)
+    # print(0)
     driver.get("https://forms.office.com/r/xkK7y3afMX")
     # time.sleep(3)  # Wait 5 seconds after loading the page
 
     # Part 1: Click on the element with class '.css-130'
-    print(1)
+    # print(1)
     # script_part1 = """
     # var interval = setInterval(function() {
     # var x = document.querySelector('.css-130');
@@ -46,17 +49,19 @@ try:
     input_field = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Enter your answer"]')
     input_field.send_keys('Impostor')
     # time.sleep(3)
-    print("step 1")
+    # print("step 1")
     # time.sleep(2)  # Wait 5 seconds before the next step
 
     # Part 3: Click on the element with class '.css-239'
-    print(3)
+    # print(3)
     # script_part3 = "var y = document.querySelector('.css-239');y.click();"
     # driver.execute_script(script_part3)
     driver.execute_script("var button = Array.from(document.querySelectorAll('button, a')).find(el => el.textContent.trim() === 'Submit');button.click()")
-    time.sleep(3)
+    # time.sleep(1)
+    print("Success")
+except:
+    print("Fail")
 
-    
 
 finally:
     # Close the browser after completing the task
